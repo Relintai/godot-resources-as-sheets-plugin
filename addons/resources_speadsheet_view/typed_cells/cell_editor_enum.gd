3240a56aa6a53158fe70b3793eb2871e1de830e3
Relintai <relintai@protonmail.com>
2023-10-09 15:21:16 +0200

    Don't error when an enum property has an invalid value.

--- addons/resources_speadsheet_view/typed_cells/cell_editor_enum.gd
+++ addons/resources_speadsheet_view/typed_cells/cell_editor_enum.gd
@@ -6,7 +6,15 @@ func can_edit_value(value, type, property_hint, column_index) -> bool:
 
 
 func set_value(node : Control, value):
-	node.text = hint_strings_array[node.get_position_in_parent() % hint_strings_array.size()][value]
+	var arr = hint_strings_array[node.get_position_in_parent() % hint_strings_array.size()]
+	var s : int = arr.size()
+	
+	if value < s && value >= 0:
+		node.text = arr[value]
+	else:
+		print("CellEditor: Invalid enum value! " + str(value))
+		node.text = str(value)
+		
 	node.self_modulate = Color(node.text.hash()) + Color(0.25, 0.25, 0.25, 1.0)
 	node.align = Label.ALIGN_CENTER
 
